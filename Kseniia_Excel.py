bd1341c33edf14128af3ce88fe6f0d79a2c0acc3
Xenia9705 <mysparkdreams@gmail.com>
2025-10-08 16:24:07 +0800

    Changes

--- Kseniia_Excel.py
+++ Kseniia_Excel.py
@@ -1,11 +1,16 @@
 #
 # Kseniia, 2025/10/08
 # File: Kseniia_Excel.py
-# Short description of the task
+# Calculate sum of column in Excel file.
 #
 
+import pandas as pd
+
 # 1. Input
+
+df = pd.read_excel('Financial_Sample.xlsx')
 
 # 2. Process
 
 # 3. Output
+print(df)
