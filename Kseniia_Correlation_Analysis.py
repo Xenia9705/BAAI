8f87bdede9886c07c6d0eb5ea205ff09da33e696
Xenia9705 <mysparkdreams@gmail.com>
2025-10-22 16:02:49 +0800

    weqw

--- Kseniia_Correlation_Analysis.py
+++ Kseniia_Correlation_Analysis.py
@@ -12,13 +12,14 @@ df = pd.read_csv('Simple_Data.csv')
 
 
 # 2. Process
-missing_values = df.isnull().sum()  # print(df.isnull().sum())
-
+# missing_values = df.isnull().sum()
+print(df.isnull().sum())
+print(df.isnull().sum().sum())
 # 3. Output
 
-print("Data loaded successfully!")
-print(f"Dataset shape: {df.shape}")
-print(df.head())
-print('Missing Values Per Column')
-print(missing_values)
-print(f"\nTotal missing values: {missing_values.sum()}")
+# print("Data loaded successfully!")
+# print(f"Dataset shape: {df.shape}")
+# print(df.head())
+# print('Missing Values Per Column')
+# print(missing_values)
+# print(f"\nTotal missing values: {missing_values.sum()}")
